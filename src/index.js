6dd334970f6113811fdaed1adb5b052c3cf47279
Saayaman <ayakosayama1987@gmail.com>
2018-08-11 03:13:26 -0700

    6. Adding Babel

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import _ from 'lodash';
 
-function component() {
+function component(name) {
   let element = document.createElement('div');
 
+  const text = `Hello, ${name}`;
+
   // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'World'], ' ');
+  element.innerHTML = _.join([text, 'World'], ' ');
 
   return element;
 }
 
-document.body.appendChild(component());
+document.body.appendChild(component('Ayako'));
--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,20 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+  mode: 'production',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015'],
+        }
+      }
+    ]
   }
 };
